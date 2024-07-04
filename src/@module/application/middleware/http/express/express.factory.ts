bd53b45d1495c512d733b4f568a8f49948ef5b20
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 14:44:51 -0300

    [ :syringe: ] | update: factory - middleware > log (module::middleware::express)

--- src/@module/application/middleware/http/express/express.factory.ts
+++ src/@module/application/middleware/http/express/express.factory.ts
@@ -1,4 +1,4 @@
-import { ErrorRequestHandler } from 'express';
+import { ErrorRequestHandler, RequestHandler } from 'express';
 import { EXPRESS_HTTP_MIDDLEWARE_MODULE } from './express.module';
 import { EXPRESS_HTTP_MIDDLEWARE_REGISTRY } from './express.registry';
 
@@ -6,4 +6,12 @@ export const EXPRESS_HTTP_MIDDLEWARE_FACTORY = {
   ERROR: EXPRESS_HTTP_MIDDLEWARE_MODULE.get<ErrorRequestHandler>(
     EXPRESS_HTTP_MIDDLEWARE_REGISTRY.ERROR,
   ),
+  LOGGER: {
+    APP: EXPRESS_HTTP_MIDDLEWARE_MODULE.get<RequestHandler>(
+      EXPRESS_HTTP_MIDDLEWARE_REGISTRY.LOGGER.APP,
+    ),
+    ERROR: EXPRESS_HTTP_MIDDLEWARE_MODULE.get<ErrorRequestHandler>(
+      EXPRESS_HTTP_MIDDLEWARE_REGISTRY.LOGGER.ERROR,
+    ),
+  },
 };
