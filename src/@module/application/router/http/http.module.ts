dc8ce0e91072836ec202ceeabb3a95a53c9d7785
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 13:25:54 -0300

    [ :syringe: ] | create: module - http (module::application::router)

---
+++ src/@module/application/router/http/http.module.ts
@@ -0,0 +1,15 @@
+import { Container } from 'inversify';
+import { HTTP_ROUTER_REGISTRY } from './http.registry';
+import { EXPRESS_APP_ROUTER } from './express/app.router';
+import { SERVER_MODULE } from '../../../infra/server/server.module';
+
+const _MODULE = new Container({
+  autoBindInjectable: true,
+  defaultScope: 'Singleton',
+});
+
+export const HTTP_ROUTER_MODULE = Container.merge(_MODULE, SERVER_MODULE);
+
+HTTP_ROUTER_MODULE.bind(HTTP_ROUTER_REGISTRY.EXPRESS.APP).toDynamicValue(
+  EXPRESS_APP_ROUTER,
+);
