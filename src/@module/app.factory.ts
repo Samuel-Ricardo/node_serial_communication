7b947ad2faef0890d56c146b9747d585cc7ad5a4
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 13:35:03 -0300

    [ :syringe: ] | update: factory - app > application (app::module)

--- src/@module/app.factory.ts
+++ src/@module/app.factory.ts
@@ -1,5 +1,7 @@
+import { APPLICATION_MODULE } from './application/application.module';
 import { INFRA_FACTORY } from './infra/infra.factory';
 
 export const MODULES = {
   INFRA: INFRA_FACTORY,
+  APPLICATION: APPLICATION_MODULE,
 };
