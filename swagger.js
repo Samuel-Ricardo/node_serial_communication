38437ebbacbf415d1e171bcc5ca0d96e39eaaa38
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 09:55:34 -0300

    [ :gear: ] | setup: config - swagger (app::config::docs)

---
+++ swagger.js
@@ -0,0 +1,18 @@
+const swaggerAutogen = require("swagger-autogen")();
+require("dotenv").config();
+
+const doc = {
+  info: {
+    title: "Serial Microsservice",
+    description: "Microsservice for Serial Communication With NodeJS",
+  },
+  host: process.env.HOST || "localhost:3000",
+  schemes: ["http", "https"],
+  consumes: ["application/json"],
+  produces: ["application/json"],
+};
+
+const outputFile = "./docs/swagger.json";
+const routes = [];
+
+swaggerAutogen(outputFile, routes, doc);
