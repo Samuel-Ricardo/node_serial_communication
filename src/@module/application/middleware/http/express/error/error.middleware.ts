3657c8b615f0560bee91c08e2e6ff2f10f11da99
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 13:51:02 -0300

    [ :goal_net: ] | create: middleware - errors (app::module::middleware)

---
+++ src/@module/application/middleware/http/express/error/error.middleware.ts
@@ -0,0 +1,18 @@
+import { ErrorRequestHandler } from 'express';
+import { AppError } from '../../../../../../@lib/error/app.error';
+
+export const ERROR_MIDDLEWARE: ErrorRequestHandler = (
+  error,
+  req,
+  res,
+  next,
+) => {
+  return error instanceof AppError
+    ? res.status(error.status).json(error.toStruct())
+    : res.status(500).json({
+        name: error.name,
+        status: 500,
+        message: error.message,
+        error: true,
+      });
+};
