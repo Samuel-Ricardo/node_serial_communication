812af1cd39ce6920d3d86ad5793609294729f3e7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 14:28:09 -0300

    [ :syringe: ] | update: factory - application > middleware (app::module)

--- src/@module/application/application.factory.ts
+++ src/@module/application/application.factory.ts
@@ -1,5 +1,7 @@
+import { MIDDLEWARE_FACTORY } from './middleware/middleware.factory';
 import { ROUTER_FACTORY } from './router/router.factory';
 
 export const APPLICATION_FACTORY = {
   ROUTER: ROUTER_FACTORY,
+  MIDDLEWARE: MIDDLEWARE_FACTORY,
 };
