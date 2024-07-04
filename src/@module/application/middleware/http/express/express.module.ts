fd74a6659fc8252d88b54612cfe25fbec9aff99c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 16:09:24 -0300

    [ :clown_face: ] | data: server - health check (test::server)

--- src/@module/application/middleware/http/express/express.module.ts
+++ src/@module/application/middleware/http/express/express.module.ts
@@ -3,7 +3,7 @@ import { EXPRESS_HTTP_MIDDLEWARE_REGISTRY } from './express.registry';
 import { ERROR_MIDDLEWARE } from './error/error.middleware';
 import { LOGGER_MIDDLEWARE } from './log/logger.middleware';
 import { ERROR_LOGGER_MIDDLEWARE } from './log/error/logger.middleware';
-import { ZOD_VALIDATOR_MIDDLEWARE } from './validation/zod/validation.middleware';
+import { ZOD_VALIDATOR_MIDDLEWARE } from './validator/zod/validation.middleware';
 
 export const EXPRESS_HTTP_MIDDLEWARE_MODULE = new Container({
   autoBindInjectable: true,
