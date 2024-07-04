54bfdabb44ea564a6c04b896808fb646f1a96670
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 14:27:57 -0300

    [ :syringe: ] | update: module - application > middleware (app::module)

--- src/@module/application/application.module.ts
+++ src/@module/application/application.module.ts
@@ -1,9 +1,14 @@
 import { Container } from 'inversify';
 import { ROUTER_MODULE } from './router/router.module';
+import { MIDDLEWARE_MODULE } from './middleware/middleware.module';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
 
-export const APPLICATION_MODULE = Container.merge(_MODULE, ROUTER_MODULE);
+export const APPLICATION_MODULE = Container.merge(
+  _MODULE,
+  ROUTER_MODULE,
+  MIDDLEWARE_MODULE,
+);
