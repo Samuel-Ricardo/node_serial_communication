e976771e5107a93a9445d48dcab2f3c720fd17ca
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 11:00:19 -0300

    [ :recycle: ] | refact: rename (module::infra)

--- src/@module/infra/server/http/http.factory.ts
+++ src/@module/infra/server/http/http.factory.ts
@@ -1,27 +1,23 @@
 import { Express, Router } from 'express';
 import { EXPRESS_CORS_TYPE } from '../../../../@types/module/infra/server/http/express/cors.type';
 import { EXPRESS_BODY_PARSER_TYPE } from '../../../../@types/module/infra/server/http/express/parser/body.type';
-import { HTTP_SERVER_ENGINE_MODULE } from './http.module';
-import { HTTP_SERVER_ENGINE_REGISTRY } from './http.registry';
+import { HTTP_SERVER_MODULE } from './http.module';
+import { HTTP_SERVER_REGISTRY } from './http.registry';
 
-export const HTTP_SERVER_ENGINE_FACTORY = {
+export const HTTP_SERVER_FACTORY = {
   EXPRESS: {
     APP: () =>
-      HTTP_SERVER_ENGINE_MODULE.get<Express>(
-        HTTP_SERVER_ENGINE_REGISTRY.EXPRESS.APP,
-      ),
+      HTTP_SERVER_MODULE.get<Express>(HTTP_SERVER_REGISTRY.EXPRESS.APP),
     CORS: () =>
-      HTTP_SERVER_ENGINE_MODULE.get<EXPRESS_CORS_TYPE>(
-        HTTP_SERVER_ENGINE_REGISTRY.EXPRESS.CORS,
+      HTTP_SERVER_MODULE.get<EXPRESS_CORS_TYPE>(
+        HTTP_SERVER_REGISTRY.EXPRESS.CORS,
       ),
     ROUTER: () =>
-      HTTP_SERVER_ENGINE_MODULE.get<Router>(
-        HTTP_SERVER_ENGINE_REGISTRY.EXPRESS.ROUTER,
-      ),
+      HTTP_SERVER_MODULE.get<Router>(HTTP_SERVER_REGISTRY.EXPRESS.ROUTER),
     PARSER: {
       BODY: () =>
-        HTTP_SERVER_ENGINE_MODULE.get<EXPRESS_BODY_PARSER_TYPE>(
-          HTTP_SERVER_ENGINE_REGISTRY.EXPRESS.PARSER.BODY,
+        HTTP_SERVER_MODULE.get<EXPRESS_BODY_PARSER_TYPE>(
+          HTTP_SERVER_REGISTRY.EXPRESS.PARSER.BODY,
         ),
     },
   },
--- src/@module/infra/server/http/http.module.ts
+++ src/@module/infra/server/http/http.module.ts
@@ -2,26 +2,26 @@ import { Container } from 'inversify';
 import { EXPRESS_CORS } from './express/express.cors';
 import { EXPRESS_ROUTER } from './express/express.router';
 import { EXPRESS_BODY_PARSER } from './express/parser/body.parser';
-import { HTTP_SERVER_ENGINE_REGISTRY } from './http.registry';
+import { HTTP_SERVER_REGISTRY } from './http.registry';
 import { EXPRESS_SERVER } from './express/express.server';
 
-export const HTTP_SERVER_ENGINE_MODULE = new Container({
+export const HTTP_SERVER_MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
 
-HTTP_SERVER_ENGINE_MODULE.bind(
-  HTTP_SERVER_ENGINE_REGISTRY.EXPRESS.APP,
-).toConstantValue(EXPRESS_SERVER);
+HTTP_SERVER_MODULE.bind(HTTP_SERVER_REGISTRY.EXPRESS.APP).toConstantValue(
+  EXPRESS_SERVER,
+);
 
-HTTP_SERVER_ENGINE_MODULE.bind(
-  HTTP_SERVER_ENGINE_REGISTRY.EXPRESS.PARSER.BODY,
+HTTP_SERVER_MODULE.bind(
+  HTTP_SERVER_REGISTRY.EXPRESS.PARSER.BODY,
 ).toConstantValue(EXPRESS_BODY_PARSER);
 
-HTTP_SERVER_ENGINE_MODULE.bind(
-  HTTP_SERVER_ENGINE_REGISTRY.EXPRESS.CORS,
-).toConstantValue(EXPRESS_CORS);
+HTTP_SERVER_MODULE.bind(HTTP_SERVER_REGISTRY.EXPRESS.CORS).toConstantValue(
+  EXPRESS_CORS,
+);
 
-HTTP_SERVER_ENGINE_MODULE.bind(
-  HTTP_SERVER_ENGINE_REGISTRY.EXPRESS.ROUTER,
-).toConstantValue(EXPRESS_ROUTER);
+HTTP_SERVER_MODULE.bind(HTTP_SERVER_REGISTRY.EXPRESS.ROUTER).toConstantValue(
+  EXPRESS_ROUTER,
+);
