72adfc4c7ebb6a0eef56e7f846d2f8cf028a94f2
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 13:34:20 -0300

    [ :syringe: ] | update: registry - app > application (app::module)

--- src/@module/app.registry.ts
+++ src/@module/app.registry.ts
@@ -1,5 +1,7 @@
+import { APPLICATION_REGISTRY } from './application/application.registry';
 import { INFRA_REGIYSTRY } from './infra/infra.registry';
 
 export const MODULE = {
   INFRA: INFRA_REGIYSTRY,
+  APPLICATION: APPLICATION_REGISTRY,
 };
