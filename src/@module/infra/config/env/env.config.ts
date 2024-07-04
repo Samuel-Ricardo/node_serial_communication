0cef8fdbd4dc1737275c3aef1e10ad9eaac03972
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 16:35:41 -0300

    [ :key: ] | create: config - env (module::infra)

---
+++ src/@module/infra/config/env/env.config.ts
@@ -0,0 +1,12 @@
+import dotnev from 'dotenv';
+
+dotnev.config();
+
+export const ENV = {
+  ...process.env,
+  PORT: process.env.PORT || 3000,
+  SERIAL: {
+    PORT: process.env.SERIAL_PORT || '/dev/tty-usbserial1',
+    BAUD_RATE: process.env.BAUD_RATE || 57600,
+  },
+};
