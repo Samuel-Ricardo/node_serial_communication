fb4b960f25245a477b1f563537589db9115f8239
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 14:27:38 -0300

    [ :syringe: ] | update: registry - application > middleware (app::module)

--- src/@module/application/application.registry.ts
+++ src/@module/application/application.registry.ts
@@ -1,5 +1,7 @@
+import { MIDDLEWARE_REGISTRY } from './middleware/middleware.registry';
 import { ROUTER_REGISTRY } from './router/router.registry';
 
 export const APPLICATION_REGISTRY = {
   ROUTER: ROUTER_REGISTRY,
+  MIDDLEWARE: MIDDLEWARE_REGISTRY,
 };
