330837960c59b693f0747fcd15fc931620ab6c79
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 22:24:57 -0300

    [ :syringe: ] | update: registry - infra > config (app::module)

--- src/@module/infra/infra.registry.ts
+++ src/@module/infra/infra.registry.ts
@@ -1,5 +1,7 @@
+import { CONFIG_REGISTRY } from './config/config.registry';
 import { SERVER_REGISTRY } from './server/server.registry';
 
 export const INFRA_REGIYSTRY = {
   SERVER: SERVER_REGISTRY,
+  CONFIG: CONFIG_REGISTRY,
 };
