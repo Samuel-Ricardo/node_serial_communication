9ab6b23a882bca2bcbd4db0392ad2f764b8f2172
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-05 00:27:49 -0300

    [ :syringe: ] | update: module - infra > docs (app::module)

--- src/@module/infra/infra.module.ts
+++ src/@module/infra/infra.module.ts
@@ -2,6 +2,7 @@ import { Container } from 'inversify';
 import { SERVER_MODULE } from './server/server.module';
 import { CONFIG_MODULE } from './config/config.module';
 import { ENGINE_MODULE } from './engine/engine.module';
+import { DOCUMENTATION_MODULE } from './documentation/documentation.module';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
@@ -13,4 +14,5 @@ export const INFRA_MODULE = Container.merge(
   SERVER_MODULE,
   CONFIG_MODULE,
   ENGINE_MODULE,
+  DOCUMENTATION_MODULE,
 );
