1085a90a50bb8bd97fb87e7f8d7eae9fcd805b3a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 23:21:39 -0300

    [ :memo: ] | setup: docs - swagger (app::config::docs)

--- swagger.js
+++ swagger.js
@@ -1,18 +1,18 @@
-const swaggerAutogen = require("swagger-autogen")();
-require("dotenv").config();
+const swaggerAutogen = require('swagger-autogen')();
+require('dotenv').config();
 
 const doc = {
   info: {
-    title: "Serial Microsservice",
-    description: "Microsservice for Serial Communication With NodeJS",
+    title: 'Serial Microsservice',
+    description: 'Microsservice for Serial Communication With NodeJS',
   },
-  host: process.env.HOST || "localhost:3000",
-  schemes: ["http", "https"],
-  consumes: ["application/json"],
-  produces: ["application/json"],
+  host: process.env.HOST || 'localhost:3000',
+  schemes: ['http', 'https'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
 };
 
-const outputFile = "./docs/swagger.json";
-const routes = [];
+const outputFile = './docs/swagger.json';
+const routes = ['./src/@module/application/router/http/express/app.router.ts'];
 
 swaggerAutogen(outputFile, routes, doc);
