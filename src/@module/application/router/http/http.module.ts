65daa88188a22b127c7ff0cf0f543774e20462e4
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-05 00:49:41 -0300

    [ :syringe: ] | update: module - http > docs [express] (module::application::router)

--- src/@module/application/router/http/http.module.ts
+++ src/@module/application/router/http/http.module.ts
@@ -2,6 +2,7 @@ import { Container } from 'inversify';
 import { HTTP_ROUTER_REGISTRY } from './http.registry';
 import { EXPRESS_APP_ROUTER } from './express/app.router';
 import { SERVER_MODULE } from '../../../infra/server/server.module';
+import { EXPRESS_DOCS_ROUTER } from './express/docs.router';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
@@ -12,4 +13,8 @@ export const HTTP_ROUTER_MODULE = Container.merge(_MODULE, SERVER_MODULE);
 
 HTTP_ROUTER_MODULE.bind(HTTP_ROUTER_REGISTRY.EXPRESS.APP).toDynamicValue(
   EXPRESS_APP_ROUTER,
+);
+
+HTTP_ROUTER_MODULE.bind(HTTP_ROUTER_REGISTRY.EXPRESS.DOCS).toDynamicValue(
+  EXPRESS_DOCS_ROUTER,
 );
