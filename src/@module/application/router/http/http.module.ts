b13234c8324973219a203bb870e05a3c50bcdd42
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-05 00:54:22 -0300

    [ :bookmark_tabs: ] | setup: route - docs > swagger (module::application)

--- src/@module/application/router/http/http.module.ts
+++ src/@module/application/router/http/http.module.ts
@@ -3,13 +3,18 @@ import { HTTP_ROUTER_REGISTRY } from './http.registry';
 import { EXPRESS_APP_ROUTER } from './express/app.router';
 import { SERVER_MODULE } from '../../../infra/server/server.module';
 import { EXPRESS_DOCS_ROUTER } from './express/docs.router';
+import { DOCUMENTATION_MODULE } from '../../../infra/documentation/documentation.module';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
 
-export const HTTP_ROUTER_MODULE = Container.merge(_MODULE, SERVER_MODULE);
+export const HTTP_ROUTER_MODULE = Container.merge(
+  _MODULE,
+  SERVER_MODULE,
+  DOCUMENTATION_MODULE,
+);
 
 HTTP_ROUTER_MODULE.bind(HTTP_ROUTER_REGISTRY.EXPRESS.APP).toDynamicValue(
   EXPRESS_APP_ROUTER,
