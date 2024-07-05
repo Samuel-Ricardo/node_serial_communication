b498c424986165fbd50668e5e43ad331ceb12357
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 22:29:19 -0300

    [ :door: ] | update: gateway - serial (module::domain)

--- src/@module/domain/gateway/serial/serial.gateway.ts
+++ src/@module/domain/gateway/serial/serial.gateway.ts
@@ -1,6 +1,8 @@
 export interface ISerialPortGateway {
   open(): Promise<void>;
   close(): Promise<void>;
+  onOpen(callback: Function): void;
+  onClose(callback: Function): void;
   read(): Promise<Buffer>;
   write(buffer: Buffer): Promise<void>;
   stream(read: Function): NodeJS.ReadWriteStream;
