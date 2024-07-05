9a04bca1d8ab72aa41084d9adba4588056be37f3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-05 11:30:17 -0300

    [ :syringe: ] | update: factory - application > gateway (app::module)

--- src/@module/application/application.factory.ts
+++ src/@module/application/application.factory.ts
@@ -1,7 +1,9 @@
+import { GATEWEAY_FACTORY } from './gateway/gatewy.factory';
 import { MIDDLEWARE_FACTORY } from './middleware/middleware.factory';
 import { ROUTER_FACTORY } from './router/router.factory';
 
 export const APPLICATION_FACTORY = {
   ROUTER: ROUTER_FACTORY,
   MIDDLEWARE: MIDDLEWARE_FACTORY,
+  GATEWAY: GATEWEAY_FACTORY,
 };
