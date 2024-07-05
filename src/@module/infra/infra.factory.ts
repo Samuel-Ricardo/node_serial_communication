19d282b7314b9f05026a9c03878d3d0a460c343d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 22:25:54 -0300

    [ :syringe: ] | update: factory - infra > config (app::module)

--- src/@module/infra/infra.factory.ts
+++ src/@module/infra/infra.factory.ts
@@ -1,5 +1,7 @@
+import { CONFIG_FACTORY } from './config/config.factory';
 import { SERVER_FACTORY } from './server/server.factory';
 
 export const INFRA_FACTORY = {
   SERVER: SERVER_FACTORY,
+  CONFIG: CONFIG_FACTORY,
 };
