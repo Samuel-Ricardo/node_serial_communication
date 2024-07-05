e978592c6fac027405ea301a952a7bd3fd960fba
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-05 00:17:59 -0300

    [ :bookmark_tabs: ] | create: docs - swagger (module::infra)

---
+++ src/@module/infra/documentation/swagger/swagger.docs.ts
@@ -0,0 +1,24 @@
+import { MODULE } from '../../../app.registry';
+import { injectable } from 'inversify';
+import { IDocumentationModule } from '../documentation.interface';
+import { RequestHandler } from 'express';
+
+import DOCUMENTATION from '../../../../../docs/swagger.json';
+import { injectEngine } from '../../engine/engine.module';
+import { SWAGGER_ENGINE_TYPE } from '../../../../@types/module/infra/engine/documentation/swagger.type';
+
+@injectable()
+export class SwaggerModule
+  implements IDocumentationModule<RequestHandler, RequestHandler>
+{
+  @injectEngine(MODULE.INFRA.ENGINE.DOCS.SWAGGER)
+  private readonly _engine!: SWAGGER_ENGINE_TYPE;
+
+  server(): RequestHandler {
+    return this._engine.serve as any;
+  }
+
+  setup(): RequestHandler {
+    return this._engine.setup(DOCUMENTATION);
+  }
+}
