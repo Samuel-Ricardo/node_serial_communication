ce923c40791203db27499debacd3482d65381acf
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 22:43:25 -0300

    [ :syringe: ] | update: module - engine > lazy (module::infra)

--- src/@module/infra/engine/engine.module.ts
+++ src/@module/infra/engine/engine.module.ts
@@ -1,9 +1,13 @@
 import { Container } from 'inversify';
 import { GATEWAY_ENGINE_MODULE } from './gateway/gateway.module';
 
+import lazy from 'inversify-inject-decorators';
+
 const _MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
 
 export const ENGINE_MODULE = Container.merge(_MODULE, GATEWAY_ENGINE_MODULE);
+
+export const { lazyInject: injectEngine } = lazy(ENGINE_MODULE);
