718c061a11ce6c2b8670b36500671b17703defdd
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 23:55:47 -0300

    [ :syringe: ] | update: factory - engine > docs (module::infra)

--- src/@module/infra/engine/engine.factory.ts
+++ src/@module/infra/engine/engine.factory.ts
@@ -1,5 +1,10 @@
+import { ENGINE_MODULE } from './engine.module';
+import { ENGINE_REGISTRY } from './engine.registry';
 import { GATEWAY_ENGINE_FACTORY } from './gateway/gateway.factory';
 
 export const ENGINE_FACTORY = {
   GATEWAY: GATEWAY_ENGINE_FACTORY,
+  DOCS: {
+    SWAGGER: () => ENGINE_MODULE.get(ENGINE_REGISTRY.DOCS.SWAGGER),
+  },
 };
