f498d02fa5af159f72edc9a0adf7d65bac4375ed
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 22:47:40 -0300

    [ :syringe: ] | update: factory - infra > engine (app::module)

--- src/@module/infra/engine/gateway/serial/serial_port.engine.ts
+++ src/@module/infra/engine/gateway/serial/serial_port.engine.ts
@@ -4,4 +4,5 @@ import { ENV } from '../../../config/env/env.config';
 export const SERIAL_PORT_GATEWAY_ENGINE = new SerialPort({
   path: ENV.SERIAL.PORT,
   baudRate: ENV.SERIAL.BAUD_RATE as number,
+  autoOpen: false,
 });
