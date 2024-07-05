fdf23baca2bb31dcf593c0a1e77105684c443f8a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 22:25:36 -0300

    [ :syringe: ] | update: module - infra > config (app::module)

--- src/@module/infra/infra.module.ts
+++ src/@module/infra/infra.module.ts
@@ -1,9 +1,14 @@
 import { Container } from 'inversify';
 import { SERVER_MODULE } from './server/server.module';
+import { CONFIG_MODULE } from './config/config.module';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
 
-export const INFRA_MODULE = Container.merge(_MODULE, SERVER_MODULE);
+export const INFRA_MODULE = Container.merge(
+  _MODULE,
+  SERVER_MODULE,
+  CONFIG_MODULE,
+);
