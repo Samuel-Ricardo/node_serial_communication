6272e45560b1c97927643e0ecf0b5219698ab45e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-04 23:27:03 -0300

    [ :syringe: ] | update: registry - engine > docs (module::infra)

--- src/@module/infra/engine/engine.registry.ts
+++ src/@module/infra/engine/engine.registry.ts
@@ -2,4 +2,7 @@ import { GATEWAY_ENGINE_REGISTRY } from './gateway/gateway.registry';
 
 export const ENGINE_REGISTRY = {
   GATEWEAY: GATEWAY_ENGINE_REGISTRY,
+  DOCS: {
+    SWAGGER: Symbol.for('MODULE::INFRA::ENGINE::DOCS::SWAGGER'),
+  },
 };
