2e55837215dba1eb9345cab7de265d237a611d6c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-05 00:51:14 -0300

    [ :syringe: ] | update: factory - http > docs [express] (module::application::router)

--- src/@module/application/router/http/http.factory.ts
+++ src/@module/application/router/http/http.factory.ts
@@ -5,5 +5,7 @@ import { HTTP_ROUTER_REGISTRY } from './http.registry';
 export const HTTP_ROUTER_FACTORY = {
   EXPRESS: {
     APP: () => HTTP_ROUTER_MODULE.get<Router>(HTTP_ROUTER_REGISTRY.EXPRESS.APP),
+    DOCS: () =>
+      HTTP_ROUTER_MODULE.get<Router>(HTTP_ROUTER_REGISTRY.EXPRESS.DOCS),
   },
 };
