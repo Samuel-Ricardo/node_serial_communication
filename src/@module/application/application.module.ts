12523d3113d62586042cca2e832802261b29573f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-05 11:29:28 -0300

    [ :syringe: ] | update: module - application > gateway (app::module)

--- src/@module/application/application.module.ts
+++ src/@module/application/application.module.ts
@@ -1,6 +1,7 @@
 import { Container } from 'inversify';
 import { ROUTER_MODULE } from './router/router.module';
 import { MIDDLEWARE_MODULE } from './middleware/middleware.module';
+import { GATEWAY_MODULE } from './gateway/gateway.module';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
@@ -11,4 +12,5 @@ export const APPLICATION_MODULE = Container.merge(
   _MODULE,
   ROUTER_MODULE,
   MIDDLEWARE_MODULE,
+  GATEWAY_MODULE,
 );
