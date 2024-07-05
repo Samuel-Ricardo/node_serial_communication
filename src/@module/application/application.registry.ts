dd9853b49de40c42b470c464ac4b35e97e0beeb9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-05 11:29:06 -0300

    [ :syringe: ] | update: registry - application > gateway (app::module)

--- src/@module/application/application.registry.ts
+++ src/@module/application/application.registry.ts
@@ -1,7 +1,9 @@
+import { GATEWAY_REGISTRY } from './gateway/gateway.registry';
 import { MIDDLEWARE_REGISTRY } from './middleware/middleware.registry';
 import { ROUTER_REGISTRY } from './router/router.registry';
 
 export const APPLICATION_REGISTRY = {
   ROUTER: ROUTER_REGISTRY,
   MIDDLEWARE: MIDDLEWARE_REGISTRY,
+  GATEWAY: GATEWAY_REGISTRY,
 };
