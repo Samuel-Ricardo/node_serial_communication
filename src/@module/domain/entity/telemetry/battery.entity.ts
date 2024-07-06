a787d6fca3eda6b4c9de97b1d70acebbdc0360be
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-05 23:38:26 -0300

    [ :sparkles: ] | create: entity - battery > toDTO (module::domain::telemetry)

--- src/@module/domain/entity/telemetry/battery.entity.ts
+++ src/@module/domain/entity/telemetry/battery.entity.ts
@@ -1,7 +1,17 @@
+import { IBatteryDTO } from '../../DTO/telemetry/battery/battery.dto';
+
 export class Battery {
   constructor(
     public readonly voltage: number,
     public readonly current: number,
     public readonly state_of_charge: number,
   ) {}
+
+  toDTO(): IBatteryDTO {
+    return {
+      voltage: this.voltage,
+      current: this.current,
+      state_of_charge: this.state_of_charge,
+    };
+  }
 }
