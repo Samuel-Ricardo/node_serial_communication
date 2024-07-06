13c6d083af17241170251e95b291d1e1f5ec02cd
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-06 11:32:53 -0300

    [ :zzz: ] | create: module - use case [lazy] (module::application)

--- src/@module/application/use_case/use_case.module.ts
+++ src/@module/application/use_case/use_case.module.ts
@@ -1,6 +1,8 @@
 import { Container } from 'inversify';
 import { TELEMETRY_USE_CASE_MODULE } from './telemetry/telemetry.module';
 
+import lazy from 'inversify-inject-decorators';
+
 const _MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
@@ -10,3 +12,5 @@ export const USE_CASE_MODULE = Container.merge(
   _MODULE,
   TELEMETRY_USE_CASE_MODULE,
 );
+
+export const { lazyInject: injectUseCase } = lazy(USE_CASE_MODULE);
