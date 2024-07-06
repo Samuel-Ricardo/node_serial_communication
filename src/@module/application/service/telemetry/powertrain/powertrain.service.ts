7b6294aaf1a56e11aba6580edb94a28d17945715
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-06 11:55:21 -0300

    [ :recycle: ] | refact: module - service (module::application::telemetry)

--- src/@module/application/service/telemetry/powertrain/powertrain.service.ts
+++ src/@module/application/service/telemetry/powertrain/powertrain.service.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from 'inversify';
-import { IPowertrainService } from '../../../domain/service/telemetry/powertrain/powertrain.service';
-import { ReadPowertrainTelemetryUseCase } from '../../use_case/telemetry/powertrain/read_powertrain_telemetry.use_case';
-import { MODULE } from '../../../app.registry';
+import { IPowertrainService } from '../../../../domain/service/telemetry/powertrain/powertrain.service';
+import { ReadPowertrainTelemetryUseCase } from '../../../use_case/telemetry/powertrain/read_powertrain_telemetry.use_case';
+import { MODULE } from '../../../../app.registry';
 
 @injectable()
 export class PowertrainService implements IPowertrainService {
