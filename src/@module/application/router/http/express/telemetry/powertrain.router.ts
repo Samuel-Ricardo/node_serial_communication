1fd29c8102ecd110b9952cb4b81a499367f655cf
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-06 13:28:01 -0300

    [ :vertical_traffic_light: ] | create: router - express > powertrain (module::application::telemetry)

---
+++ src/@module/application/router/http/express/telemetry/powertrain.router.ts
@@ -0,0 +1,24 @@
+import { interfaces } from 'inversify';
+import { MODULE } from '../../../../../app.registry';
+import { PowertrainController } from '../../../../controller/telemetry/powertrain/powertrain.controller';
+import { Router } from 'express';
+import { AppError } from '../../../../../../@lib/error/app.error';
+
+export const POWERTRAIN_ROUTER = ({ container }: interfaces.Context) => {
+  const ROUTER = container.get<Router>(MODULE.INFRA.SERVER.HTTP.EXPRESS.ROUTER);
+  const CONTROLLER = container.get<PowertrainController>(
+    MODULE.APPLICATION.CONTROLLER.TELEMETRY.POWERTRAIN,
+  );
+
+  ROUTER.get('/powertrain', (req, res) => {
+    try {
+      return CONTROLLER.getPowertrainTelemetry();
+    } catch (error) {
+      if (error instanceof AppError)
+        return res.status(error.status).send(error);
+      return res.status(500).send(error);
+    }
+  });
+
+  return ROUTER;
+};
