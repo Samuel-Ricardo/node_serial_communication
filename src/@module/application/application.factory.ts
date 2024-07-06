6d72cc502713facacc645d5ffc09233bb23c3629
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-06 12:39:10 -0300

    [ :syringe: ] | update: factory - application > service (app::module)

--- src/@module/application/application.factory.ts
+++ src/@module/application/application.factory.ts
@@ -1,6 +1,7 @@
 import { GATEWEAY_FACTORY } from './gateway/gateway.factory';
 import { MIDDLEWARE_FACTORY } from './middleware/middleware.factory';
 import { ROUTER_FACTORY } from './router/router.factory';
+import { SERVICE_FACTORY } from './service/service.factory';
 import { USE_CASE_FACTORY } from './use_case/use_case.factory';
 
 export const APPLICATION_FACTORY = {
@@ -8,4 +9,5 @@ export const APPLICATION_FACTORY = {
   MIDDLEWARE: MIDDLEWARE_FACTORY,
   GATEWAY: GATEWEAY_FACTORY,
   USE_CASE: USE_CASE_FACTORY,
+  SERVICE: SERVICE_FACTORY,
 };
--- src/@module/application/service/service.factory.ts
+++ src/@module/application/service/service.factory.ts
@@ -1,10 +1,5 @@
-import { IPowertrainService } from '../../domain/service/telemetry/powertrain/powertrain.service';
-import { TELEMETRY_SERVICE_MODULE } from './telemetry/telemetry.module';
-import { TELEMETRY_SERVICE_REGISTRY } from './telemetry/telemetry.registry';
+import { TELEMETRY_SERVICE_FACTORY } from './telemetry/telemetry.factory';
 
-export const TELEMETRY_SERVICE_FACTORY = {
-  POWERTRAIN: () =>
-    TELEMETRY_SERVICE_MODULE.get<IPowertrainService>(
-      TELEMETRY_SERVICE_REGISTRY.POWERTRAIN,
-    ),
+export const SERVICE_FACTORY = {
+  TELEMETRY: TELEMETRY_SERVICE_FACTORY,
 };
