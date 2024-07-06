ffeaf4e2ec30109d3248133db1c3f000d4607250
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-06 12:43:45 -0300

    [ :video_game: ] | create: controller - powertrain (module::application::telemetry)

---
+++ src/@module/application/controller/telemetry/powertrain/powertrain.controller.ts
@@ -0,0 +1,11 @@
+import { inject, injectable } from 'inversify';
+import { IPowertrainService } from '../../../../domain/service/telemetry/powertrain/powertrain.service';
+import { MODULE } from '../../../../app.registry';
+
+@injectable()
+export class PowertrainController {
+  constructor(
+    @inject(MODULE.APPLICATION.SERVICE.TELEMETRY.POWERTRAIN)
+    private readonly service: IPowertrainService,
+  ) {}
+}
