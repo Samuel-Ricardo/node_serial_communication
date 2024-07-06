9bf8a844fa07b12c543e945b8d24989e529acdc6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-06 11:49:37 -0300

    [ :syringe: ] | update: factory - application > use case (app::module)

--- src/@module/application/application.factory.ts
+++ src/@module/application/application.factory.ts
@@ -1,9 +1,11 @@
 import { GATEWEAY_FACTORY } from './gateway/gateway.factory';
 import { MIDDLEWARE_FACTORY } from './middleware/middleware.factory';
 import { ROUTER_FACTORY } from './router/router.factory';
+import { USE_CASE_FACTORY } from './use_case/use_case.factory';
 
 export const APPLICATION_FACTORY = {
   ROUTER: ROUTER_FACTORY,
   MIDDLEWARE: MIDDLEWARE_FACTORY,
   GATEWAY: GATEWEAY_FACTORY,
+  USE_CASE: USE_CASE_FACTORY,
 };
