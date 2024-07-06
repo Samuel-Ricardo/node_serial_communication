e12499563c8a79d3c932e4c4f6242ded639397f0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-06 13:31:55 -0300

    [ :syringe: ] | update: factory - http > powertrain (module::application::router)

--- src/@module/application/router/http/http.factory.ts
+++ src/@module/application/router/http/http.factory.ts
@@ -7,5 +7,11 @@ export const HTTP_ROUTER_FACTORY = {
     APP: () => HTTP_ROUTER_MODULE.get<Router>(HTTP_ROUTER_REGISTRY.EXPRESS.APP),
     DOCS: () =>
       HTTP_ROUTER_MODULE.get<Router>(HTTP_ROUTER_REGISTRY.EXPRESS.DOCS),
+    TELEMETRY: {
+      POWERTRAIN: () =>
+        HTTP_ROUTER_MODULE.get<Router>(
+          HTTP_ROUTER_REGISTRY.EXPRESS.TELEMETRY.POWERTRAIN,
+        ),
+    },
   },
 };
