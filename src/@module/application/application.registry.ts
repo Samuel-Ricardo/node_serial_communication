d6d936244dc1a64228a94d10f5b1c316b7cb9a6f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-06 11:38:57 -0300

    [ :syringe: ] | update: registry - application > use case (app::module)

--- src/@module/application/application.registry.ts
+++ src/@module/application/application.registry.ts
@@ -1,9 +1,11 @@
 import { GATEWAY_REGISTRY } from './gateway/gateway.registry';
 import { MIDDLEWARE_REGISTRY } from './middleware/middleware.registry';
 import { ROUTER_REGISTRY } from './router/router.registry';
+import { USE_CASE_REGISTRY } from './use_case/use_case.registry';
 
 export const APPLICATION_REGISTRY = {
   ROUTER: ROUTER_REGISTRY,
   MIDDLEWARE: MIDDLEWARE_REGISTRY,
   GATEWAY: GATEWAY_REGISTRY,
+  USE_CASE: USE_CASE_REGISTRY,
 };
