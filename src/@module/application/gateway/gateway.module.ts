29fac9ab878fc73d9c9e8da6175b1401c340fdf0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-06 00:38:18 -0300

    [ :zzz: ] | update: module - gateway [lazy] (module::application)

--- src/@module/application/gateway/gateway.module.ts
+++ src/@module/application/gateway/gateway.module.ts
@@ -1,9 +1,13 @@
 import { Container } from 'inversify';
 import { SERIAL_GATEWAY_MODULE } from './serial/serial.module';
 
+import lazy from 'inversify-inject-decorators';
+
 const _MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
 
 export const GATEWAY_MODULE = Container.merge(_MODULE, SERIAL_GATEWAY_MODULE);
+
+export const { lazyInject: injectGateway } = lazy(GATEWAY_MODULE);
