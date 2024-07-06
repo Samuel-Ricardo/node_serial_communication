d00c515ffa9de3b211da59d14594b097b9020738
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-05 23:40:52 -0300

    [ :sparkles: ] | create: entity - geography > toDTO (module::domain::telemetry)

--- src/@module/domain/entity/telemetry/geography.entity.ts
+++ src/@module/domain/entity/telemetry/geography.entity.ts
@@ -1,7 +1,17 @@
+import { IGeographyDTO } from '../../DTO/telemetry/geography/geography.dto';
+
 export class Geography {
   constructor(
     public readonly latitude: number,
     public readonly longitude: number,
     public readonly timestamp: number,
   ) {}
+
+  toDTO(): IGeographyDTO {
+    return {
+      latitude: this.latitude,
+      longitude: this.longitude,
+      timestamp: this.timestamp,
+    };
+  }
 }
