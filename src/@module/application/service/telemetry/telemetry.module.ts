0bf007582fd016cb5208d62eb95e8a9729ae970e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-06 12:03:10 -0300

    [ :syringe: ] | create: module - telemetry (module::application::service)

---
+++ src/@module/application/service/telemetry/telemetry.module.ts
@@ -0,0 +1,12 @@
+import { Container } from 'inversify';
+import { USE_CASE_MODULE } from '../../use_case/use_case.module';
+
+const _MODULE = new Container({
+  autoBindInjectable: true,
+  defaultScope: 'Singleton',
+});
+
+export const TELEMETRY_USE_CASE_MODULE = Container.merge(
+  _MODULE,
+  USE_CASE_MODULE,
+);
