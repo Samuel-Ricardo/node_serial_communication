05c217a005c968066d2edec7848d2d1a4da18dc0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-12 09:36:53 -0300

    [ :syringe: ] | update: module - telemetry > battery (module::application::gateway)

--- src/@module/application/gateway/telemetry/telemetry.module.ts
+++ src/@module/application/gateway/telemetry/telemetry.module.ts
@@ -2,6 +2,7 @@ import { Container } from 'inversify';
 import { SERIAL_TELEMETRY_GATEWAY_REGISTRY } from './telemetry.registy';
 import { SerialPortPowertrainGateway } from './powertrain/powertrain.gateway';
 import { SERIAL_GATEWAY_MODULE } from '../serial/serial.module';
+import { SerialPortBatteryGateway } from './battery/battery.gateway';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
@@ -15,3 +16,7 @@ export const SERIAL_TELEMETRY_GATEWAY_MODULE = Container.merge(
 SERIAL_TELEMETRY_GATEWAY_MODULE.bind(
   SERIAL_TELEMETRY_GATEWAY_REGISTRY.POWERTRAIN,
 ).to(SerialPortPowertrainGateway);
+
+SERIAL_TELEMETRY_GATEWAY_MODULE.bind(
+  SERIAL_TELEMETRY_GATEWAY_REGISTRY.BATTERY,
+).to(SerialPortBatteryGateway);
