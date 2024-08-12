291d3ce7c1898818267a81545c021cfe561395e1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-12 09:48:19 -0300

    [ :door: ] | create: support - gateway > battery (module::application)

--- src/@module/application/use_case/telemetry/battery/read_battery_telemetry.use_case.ts
+++ src/@module/application/use_case/telemetry/battery/read_battery_telemetry.use_case.ts
@@ -2,16 +2,13 @@ import { inject, injectable } from 'inversify';
 import { IReadBatteryTelemetryUseCase } from '../../../../domain/use_case/telemetry/battery/read_battery_telemetry.use_case';
 import { IBatteryGateway } from '../../../../domain/gateway/telemetry/battery/battery.gateway';
 import { MODULE } from '../../../../app.registry';
+import { BatterySerialGatewaySupport } from '../../../support/gateway/serial/telemetry/battery.support';
 
 @injectable()
 export class ReadBatteryTelemetryUseCase
+  extends BatterySerialGatewaySupport
   implements IReadBatteryTelemetryUseCase
 {
-  constructor(
-    @inject(MODULE.APPLICATION.GATEWAY.TELEMETRY.BATTERY)
-    private readonly gateway: IBatteryGateway,
-  ) {}
-
   async execute() {
     return await this.gateway.readBatteryTelemetry();
   }
