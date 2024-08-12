df6db1e0edac4f231581397521aec922097744a2
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-12 10:04:20 -0300

    [ :syringe: ] | update: module - telemetry > battery (module::application::service)

--- src/@module/application/service/telemetry/telemetry.module.ts
+++ src/@module/application/service/telemetry/telemetry.module.ts
@@ -2,6 +2,7 @@ import { Container } from 'inversify';
 import { USE_CASE_MODULE } from '../../use_case/use_case.module';
 import { TELEMETRY_SERVICE_REGISTRY } from './telemetry.registry';
 import { PowertrainService } from './powertrain/powertrain.service';
+import { BatteryService } from './battery/battery.service';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
@@ -15,4 +16,8 @@ export const TELEMETRY_SERVICE_MODULE = Container.merge(
 
 TELEMETRY_SERVICE_MODULE.bind(TELEMETRY_SERVICE_REGISTRY.POWERTRAIN).to(
   PowertrainService,
+);
+
+TELEMETRY_SERVICE_MODULE.bind(TELEMETRY_SERVICE_REGISTRY.BATTERY).to(
+  BatteryService,
 );
