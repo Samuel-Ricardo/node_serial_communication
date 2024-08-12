72c665d4bf04f048a39e1eafcc7f0c14ddef07a0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-12 10:16:40 -0300

    [ :syringe: ] | update: factory - telemetry > battery (module::application::controller)

--- src/@module/application/controller/telemetry/telemetry.factory.ts
+++ src/@module/application/controller/telemetry/telemetry.factory.ts
@@ -1,3 +1,4 @@
+import { BatteryController } from './battery/battery.controller';
 import { PowertrainController } from './powertrain/powertrain.controller';
 import { TELEMETRY_CONTROLLER_MODULE } from './telemetry.module';
 import { TELEMETRY_CONTROLLER_REGISTRY } from './telemetry.registry';
@@ -6,5 +7,10 @@ export const TELEMETRY_CONTROLLER_FACTORY = {
   POWERTRAIN: () =>
     TELEMETRY_CONTROLLER_MODULE.get<PowertrainController>(
       TELEMETRY_CONTROLLER_REGISTRY.POWERTRAIN,
+    ),
+
+  BATTERY: () =>
+    TELEMETRY_CONTROLLER_MODULE.get<BatteryController>(
+      TELEMETRY_CONTROLLER_REGISTRY.BATTERY,
     ),
 };
