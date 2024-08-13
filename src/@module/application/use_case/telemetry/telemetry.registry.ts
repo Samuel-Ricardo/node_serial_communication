81263ca73448768ab6c55189d3ec29a0c2f7bda9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-12 21:18:16 -0300

    [ :syringe: ] | update: registry - telemetry > geography (module::application::use_case)

--- src/@module/application/use_case/telemetry/telemetry.registry.ts
+++ src/@module/application/use_case/telemetry/telemetry.registry.ts
@@ -7,4 +7,9 @@ export const TELEMETRY_USE_CASE_REGISTRY = {
   BATTERY: {
     READ: Symbol.for('MODULE::APPLICATION::USE_CASE::TELEMETRY::BATTERY::READ'),
   },
+  GEOGRAPHY: {
+    READ: Symbol.for(
+      'MODULE::APPLICATION::USE_CASE::TELEMETRY::GEOGRAPHY::READ',
+    ),
+  },
 };
