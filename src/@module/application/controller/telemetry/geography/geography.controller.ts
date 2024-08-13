51d63dcc83a6c4d8a1d5edeb9865c6014ee8c49a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-12 21:50:28 -0300

    [ :vertical_traffic_light: ] | create: router - express > geography (module::application::telemetry)

--- src/@module/application/controller/telemetry/geography/geography.controller.ts
+++ src/@module/application/controller/telemetry/geography/geography.controller.ts
@@ -10,7 +10,7 @@ export class GeographyController {
     private readonly service: IGeographyService,
   ) {}
 
-  async read(): Promise<Geography[]> {
+  async getTelemetry(): Promise<Geography[]> {
     return await this.service.read();
   }
 }
