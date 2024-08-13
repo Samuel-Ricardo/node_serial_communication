8faa22c438ece8a72ccf23152d7082de735f574c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-12 21:39:18 -0300

    [ :video_game: ] | create: controller - geography (module::application::telemetry)

---
+++ src/@module/application/controller/telemetry/geography/geography.controller.ts
@@ -0,0 +1,16 @@
+import { inject, injectable } from 'inversify';
+import { Geography } from '../../../../domain/entity/telemetry/geography.entity';
+import { IGeographyService } from '../../../../domain/service/telemetry/geography/geography.service';
+import { MODULE } from '../../../../app.registry';
+
+@injectable()
+export class GeographyGateway {
+  constructor(
+    @inject(MODULE.APPLICATION.SERVICE.TELEMETRY.GEOGRAPHY)
+    private readonly service: IGeographyService,
+  ) {}
+
+  async read(): Promise<Geography[]> {
+    return await this.service.read();
+  }
+}
