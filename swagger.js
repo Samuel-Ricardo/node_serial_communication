c045ec1c0ec789cf248f17beec766c45f6f3f323
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-12 22:08:25 -0300

    [ :bookmark_tabs: ] | update: docs - telemetry > geography (app::config)

--- swagger.js
+++ swagger.js
@@ -16,10 +16,14 @@ const outputFile = './docs/swagger.json';
 const routes = [
   './src/@module/application/router/http/express/app.router.ts',
   './src/@module/application/router/http/express/docs.router.ts',
+
   './src/@module/application/router/http/express/telemetry/powertrain.router.ts',
   './src/@module/application/router/http/express/telemetry/battery.router.ts',
+  './src/@module/application/router/http/express/telemetry/geography.router.ts',
+
   './src/@module/application/router/http/sse/express/telemetry/powertrain.router.ts',
   './src/@module/application/router/http/sse/express/telemetry/battery.router.ts',
+  './src/@module/application/router/http/sse/express/telemetry/geography.router.ts',
 ];
 
 swaggerAutogen(outputFile, routes, doc);
