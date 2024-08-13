23ab10b2859e94923b0578426a63e8aba4af6281
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-12 21:02:41 -0300

    [ :syringe: ] | update: factory - telemetry > geography (module::application::gateway)

--- src/@module/application/gateway/telemetry/telemetry.factory.ts
+++ src/@module/application/gateway/telemetry/telemetry.factory.ts
@@ -1,4 +1,5 @@
 import { IBatteryGateway } from '../../../domain/gateway/telemetry/battery/battery.gateway';
+import { IGeographyGateway } from '../../../domain/gateway/telemetry/geography/geography.gateway';
 import { IPowertrainGateway } from '../../../domain/gateway/telemetry/powertrain/powertrain.gateway';
 import { SERIAL_TELEMETRY_GATEWAY_MODULE } from './telemetry.module';
 import { SERIAL_TELEMETRY_GATEWAY_REGISTRY } from './telemetry.registy';
@@ -12,5 +13,10 @@ export const SERIAL_TELEMETRY_GATEWAY_FACTORY = {
   BATTERY: () =>
     SERIAL_TELEMETRY_GATEWAY_MODULE.get<IBatteryGateway>(
       SERIAL_TELEMETRY_GATEWAY_REGISTRY.BATTERY,
+    ),
+
+  GEOGRAPHY: () =>
+    SERIAL_TELEMETRY_GATEWAY_MODULE.get<IGeographyGateway>(
+      SERIAL_TELEMETRY_GATEWAY_REGISTRY.GEOGRAPHY,
     ),
 };
