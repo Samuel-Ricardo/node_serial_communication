149ec2f97ca88287171e24f93401aec830c26281
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-14 00:04:22 -0300

    [ :syringe: ] | update: factory - telemetry > gyroscope (module::application::use_case)

--- src/@module/application/use_case/telemetry/telemetry.factory.ts
+++ src/@module/application/use_case/telemetry/telemetry.factory.ts
@@ -1,5 +1,6 @@
 import { IReadBatteryTelemetryUseCase } from '../../../domain/use_case/telemetry/battery/read_battery_telemetry.use_case';
 import { IReadGeographyTelemetryUseCase } from '../../../domain/use_case/telemetry/geography/read_geography_telemetry.use_case';
+import { IReadGyroscopeTelemetryUseCase } from '../../../domain/use_case/telemetry/gyroscope/read_gyroscope_telemetry.use_case';
 import { IReadPowertrainTelemetryUseCase } from '../../../domain/use_case/telemetry/powertrain/read_powertrain_telemetry.use_case';
 import { TELEMETRY_USE_CASE_MODULE } from './telemetry.module';
 import { TELEMETRY_USE_CASE_REGISTRY } from './telemetry.registry';
@@ -23,6 +24,13 @@ export const TELEMETRY_USE_CASE_FACTORY = {
     READ: () =>
       TELEMETRY_USE_CASE_MODULE.get<IReadGeographyTelemetryUseCase>(
         TELEMETRY_USE_CASE_REGISTRY.GEOGRAPHY.READ,
+      ),
+  },
+
+  GYROSCOPE: {
+    READ: () =>
+      TELEMETRY_USE_CASE_MODULE.get<IReadGyroscopeTelemetryUseCase>(
+        TELEMETRY_USE_CASE_REGISTRY.GYROSCOPE.READ,
       ),
   },
 };
