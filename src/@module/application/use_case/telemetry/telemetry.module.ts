e23272e670fad74c6709b4e641142bbeba9b2cf1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-13 23:25:45 -0300

    [ :syringe: ] | update: module - telemetry > gyroscope (module::application::use_case)

--- src/@module/application/use_case/telemetry/telemetry.module.ts
+++ src/@module/application/use_case/telemetry/telemetry.module.ts
@@ -3,6 +3,8 @@ import { TELEMETRY_USE_CASE_REGISTRY } from './telemetry.registry';
 import { ReadPowertrainTelemetryUseCase } from './powertrain/read_powertrain_telemetry.use_case';
 import { ReadBatteryTelemetryUseCase } from './battery/read_battery_telemetry.use_case';
 import { ReadGeographyTelemetryUseCase } from './geography/read/telemetry/geography.use_case';
+import { TELEMETRY_CONTROLLER_MODULE } from '../../controller/telemetry/telemetry.module';
+import { ReadGyroscopeTelemetryUseCase } from './gyroscope/read_gyroscope_telemetry.use_case';
 
 export const TELEMETRY_USE_CASE_MODULE = new Container({
   autoBindInjectable: true,
@@ -19,4 +21,8 @@ TELEMETRY_USE_CASE_MODULE.bind(TELEMETRY_USE_CASE_REGISTRY.BATTERY.READ).to(
 
 TELEMETRY_USE_CASE_MODULE.bind(TELEMETRY_USE_CASE_REGISTRY.GEOGRAPHY.READ).to(
   ReadGeographyTelemetryUseCase,
+);
+
+TELEMETRY_USE_CASE_MODULE.bind(TELEMETRY_USE_CASE_REGISTRY.GYROSCOPE.READ).to(
+  ReadGyroscopeTelemetryUseCase,
 );
