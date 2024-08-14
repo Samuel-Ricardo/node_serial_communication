9e68b07b842ce89e641117da7e9470a59fffe884
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-14 00:25:54 -0300

    [ :syringe: ] | update: module - telemetry > gyroscope (module::application::controller)

--- src/@module/application/controller/telemetry/telemetry.module.ts
+++ src/@module/application/controller/telemetry/telemetry.module.ts
@@ -4,6 +4,7 @@ import { TELEMETRY_CONTROLLER_REGISTRY } from './telemetry.registry';
 import { PowertrainController } from './powertrain/powertrain.controller';
 import { BatteryController } from './battery/battery.controller';
 import { GeographyController } from './geography/geography.controller';
+import { GyroscopeController } from './gyroscope/gyroscope.controller';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
@@ -25,4 +26,8 @@ TELEMETRY_CONTROLLER_MODULE.bind(TELEMETRY_CONTROLLER_REGISTRY.BATTERY).to(
 
 TELEMETRY_CONTROLLER_MODULE.bind(TELEMETRY_CONTROLLER_REGISTRY.GEOGRAPHY).to(
   GeographyController,
+);
+
+TELEMETRY_CONTROLLER_MODULE.bind(TELEMETRY_CONTROLLER_REGISTRY.GYROSCOPE).to(
+  GyroscopeController,
 );
